96250e179c906e498fcbc7fbf56f0f1f3cd73f16
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-17 18:46:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dzshka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите слово: ");
+            string txt = Console.ReadLine();
+
+            List<Person> people = new List<Person>() { };
+            people.Add(new Person() { Text = txt });
+
+            foreach (Person p in people)
+            {
+                Console.WriteLine(p.Text);
+            }
+        }
+    }
+
+    class Person
+    {
+        public string Text { get; set; }
+    }
+}
